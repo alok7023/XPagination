2b2e99b954c28f5f0970089575d2dd7e48cb21e4
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-01-22 01:46:59 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
+const URL = `https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json`;
 function App() {
+  const [tableData, setTableData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+
+  const fetchData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setTableData(data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = tableData.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const handlePageChange = (newPage) => setCurrentPage(newPage);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Employee Data Table</h1>
+      <table className="custom-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((val, key) => (
+            <tr key={key}>
+              <th>{val.id}</th>
+              <th>{val.name}</th>
+              <th>{val.email}</th>
+              <th>{val.role}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pagination-container">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
         >
-          Learn React
-        </a>
-      </header>
+          Prev
+        </button>
+        <span className="current-page">{currentPage}</span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={indexOfLastItem >= tableData.length}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
